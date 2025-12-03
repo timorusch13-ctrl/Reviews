820302a0cb83342c53da22d04d36f2e76f33dc2f
timorusch13-ctrl <timo.rusch13@gmail.com>
2025-12-03 20:49:36 +0100

    Create now.js

---
+++ now.js
@@ -0,0 +1,7 @@
+const firebaseConfig = {
+    // Bitte ersetzen Sie diese Platzhalter durch IHRE Werte aus der Firebase Console!
+    apiKey: "IHR_API_KEY", 
+    authDomain: "IHR_PROJEKT_ID.firebaseapp.com",
+    projectId: "IHR_PROJEKT_ID", 
+    // ... weitere Platzhalter
+};
